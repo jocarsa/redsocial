89a6777bbe5e7b4bb83f89d5d853138588cddb2b
jocarsa <info@josevicentecarratala.com>
2023-12-14 13:36:45 +0100

    a

--- 004-Base de datos/007-Insertar usuarios.sql
+++ 004-Base de datos/007-Insertar usuarios.sql
@@ -1,1 +1,4 @@
 INSERT INTO `usuarios` (`Identificador`, `nombreusuario`, `contraseña`, `nombrepropio`, `apellidos`, `email`, `foto`) VALUES (NULL, 'josevicente', 'josevicente', 'Jose Vicente', 'Carratala Sanchis', 'info@josevicentecarratala.com', 'josevicente.jpg');
+INSERT INTO `usuarios` (`Identificador`, `nombreusuario`, `contraseña`, `nombrepropio`, `apellidos`, `email`, `foto`) VALUES (NULL, 'cristina', 'perezmoreno', 'Cristina', 'Perez Moreno', 'info@cristina.com', 'cristina.jpg');
+INSERT INTO `usuarios` (`Identificador`, `nombreusuario`, `contraseña`, `nombrepropio`, `apellidos`, `email`, `foto`) VALUES (NULL, 'andrea', 'vinuesadominguez', 'Andrea', 'Vinuesa Dominguez', 'info@andrea.com', 'andrea.jpg');
+INSERT INTO `usuarios` (`Identificador`, `nombreusuario`, `contraseña`, `nombrepropio`, `apellidos`, `email`, `foto`) VALUES (NULL, 'heyber', 'correasuarez', 'Heyber', 'Correa Suarez', 'info@heyber.com', 'heyber.jpg');
